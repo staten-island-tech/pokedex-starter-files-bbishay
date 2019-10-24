5294f8f96bc174c1e72f35ad2cbfc875be69c2a6
bbishay <50244538+bbishay@users.noreply.github.com>
2019-10-24 11:45:23 -0400

    finished

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const DOMStrings = {
     displayImageBack: document.querySelector('.display-image-back-def'),
     displayImageShinyFront: document.querySelector('.display-image-shiny-front'),
     displayImageShinyBack: document.querySelector('.display-image-shiny-back'),
+    type: document.querySelector('.type'),
     displayNum: document.querySelector('.pkmn-num')
 };
+
+DOMStrings.input.addEventListener("keypress", function(event){
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('.nes-btn').click();
+    }
+});
 
 function getPkmn(){
     DOMStrings.input.addEventListener('submit', async function(e){
@@ -19,8 +27,18 @@ function getPkmn(){
             const data = await result.json();
             //console.log(data);
             const displayPkmn = function(data){
+                DOMStrings.displayName.innerText = data.name;
+                DOMStrings.displayNum.innerText = data.id;
+                DOMStrings.displayImageFront.src = data.sprites.front_default;
+                DOMStrings.displayImageBack.src = data.sprites.back_default;
+                DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
+                DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
+                DOMStrings.type.textContent = data.types.map(data => data.type.name);
+                console.log(data.types);
                 
-            }
+            };
+            displayPkmn(data);
+            DOMStrings.name.value = '';
         } catch(err){
             console.log(err);
             
